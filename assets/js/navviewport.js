c57fa4bb3de3c1a744e9c360cda4b08f1195c37f
Potato22 <potatooosss22@gmail.com>
2023-12-11 23:48:34 +0700

    wow that took longer than expected.

--- assets/js/navviewport.js
+++ assets/js/navviewport.js
@@ -30,21 +30,21 @@ $(function () {
 function chapter() {
     //calc offset
     var about = $('#about').offset().top - $(window).scrollTop();
-
-    var create = $('#threed').offset().top - $(window).scrollTop();
-    var gall = $('#designimit').offset().top - $(window).scrollTop();
-    var comm = $('#illus').offset().top - $(window).scrollTop();
-    var trivia = $('#trivia').offset().top - $(window).scrollTop();
+    //var creations = $('#creations').offset().top - $(window).scrollTop();
+    var threed = $('#threed').offset().top - $(window).scrollTop();
+    var design = $('#design').offset().top - $(window).scrollTop();
+    var illus = $('#illus').offset().top - $(window).scrollTop();
+    //var trivia = $('#trivia').offset().top - $(window).scrollTop();
     
     
 
     //BOOL
     var aboutbool;
-    var createbool;
-    var contactbool;
-    var gallbool;
-    var commbool;
-    var triviabool;
+    var threedbool;
+    var designbool;
+    var illusbool;
+    //var contactbool;
+    //var triviabool;
 
     if (about <= 0) {
         aboutbool = true;
@@ -53,36 +53,34 @@ function chapter() {
         aboutbool = false;
     }
 
-    if (create <= 0) {
-        createbool = true;
+    if (threed <= 0) {
+        threedbool = true;
         aboutbool = false;
-        //console.log("create active")
     } else {
-        createbool = false;
+        threedbool = false;
     }
-    if (gall <= 0) {
-        gallbool = true;
-        //console.log("create active")
+    if (design <= 0) {
+        threedbool = false;
+        designbool = true;
     } else {
-        gallbool = false;
+        designbool = false;
     }
-    if (comm <= 0) {
-        commbool = true;
-        gallbool = false;
-        //console.log("create active")
+    if (illus <= 0) {
+        illusbool = true;
+        designbool = false;
     } else {
-        commbool = false;
+        illusbool = false;
     }
-    if (trivia <= 0) {
-        triviabool = true;
-        commbool = false;
-        //console.log("create active")
-    } else {
-        triviabool = false;
-    }
+    //if (trivia <= 0) {
+    //    triviabool = true;
+    //    illusbool = false;
+    //    //console.log("threed active")
+    //} else {
+    //    triviabool = false;
+    //}
 
-    console.log('creatbool', createbool, '\n gallbool', gallbool, '\n commbool', commbool, '\n triviabool', triviabool )
-    console.log('createbool %s, gallbool %s, triviabool %s', createbool, gallbool, triviabool)
+    //console.log('creatbool', threedbool, '\n designbool', designbool, '\n illusbool', illusbool, '\n triviabool', triviabool )
+    console.log('aboutbool %s, threedbool %s, designbool %s, triviabool %s', aboutbool, threedbool, designbool, illusbool)
 
     if (aboutbool == true) {
         $('.s1').addClass('activestrike')
@@ -90,34 +88,26 @@ function chapter() {
         $('.s1').removeClass('activestrike')
     }
 
-    if (createbool == true) {
+    if (threedbool || designbool || illusbool == true) {
         $('.s2').addClass('activestrike')
     } else {
         $('.s2').removeClass('activestrike')
     }
-    if (gallbool == true) {
+    
+    if (threedbool == true) {
         $('.s2-1').addClass('blockactive')
     } else {
         $('.s2-1').removeClass('blockactive')
     }
-
-    if (commbool == true) {
+    if (designbool == true) {
         $('.s2-2').addClass('blockactive')
     } else {
         $('.s2-2').removeClass('blockactive')
     }
-
-    if (triviabool == true) {
+    if (illusbool == true) {
         $('.s2-3').addClass('blockactive')
     } else {
         $('.s2-3').removeClass('blockactive')
-    }
-
-
-    if (contactbool == true) {
-        $('.s3').addClass('activestrike')
-    } else {
-        $('.s3').removeClass('activestrike')
     }
 }
 chapter();
--- assets/js/zoomtrigger.js
+++ assets/js/zoomtrigger.js
@@ -1,14 +1,28 @@
 function zooming() {
     $(".zoomindimmer").fadeIn(200), $(".zoomin").show(), $(".dynamicnav").addClass("navHide")
 }
+
 function zoomout() {
-    $(".zoomincontent").attr("src", ""), $(".zoomindimmer").fadeOut(200), $(".zoomin").hide(), $(".dynamicnav").removeClass("navHide")
+    $(".zoomincontent").attr("src", "").removeClass('zoomDisable'), $(".zoomindimmer").fadeOut(200), $(".zoomin").hide(), $(".dynamicnav").removeClass("navHide")
+    $(".zoomalttext").html("")
 }
 document.getElementById("daImg").addEventListener("galleryLoaded", (function () {
     console.log('ready')
-    $(".daImgs, .imgz").click((function () {
-        $(".zoomincontent").attr("src", $(this).attr("src"))
-        zooming();
+    $(".daImgs, img.imgz, video.imgz").click((function () {
+        var zoomRequestType = $(this);
+
+        if (zoomRequestType.is('img.imgz')) {
+            $("video.zoomincontent").addClass('zoomDisable')
+            $("img.zoomincontent").attr("src", $(this).attr("src"))
+            $(".zoomalttext").html($(this).attr("alt"))
+            zooming();
+        } else if (zoomRequestType.is('video.imgz')) {
+            $("img.zoomincontent").addClass('zoomDisable')
+            $("video.zoomincontent").attr("src", $(this).attr("src"))
+            $(".zoomalttext").html($(this).attr("alt"))
+            zooming();
+        }
+        //console.log(zoomRequestType)
     }));
     $(".zoomincontent,.zoomin").click((function () {
         zoomout();
